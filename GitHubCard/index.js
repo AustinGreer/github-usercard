47617f4886874389776615d9a9574e2128cd661e
Austin Greer <agreer.ag12@gmail.com>
2021-02-11 17:15:53 -0500

    successfully made get request, built usercard component

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -1,9 +1,22 @@
+import axios from "axios"
+
 /*
   STEP 1: using axios, send a GET request to the following URL
     (replacing the placeholder with your Github name):
     https://api.github.com/users/<your name>
 */
-import axios from "axios"
+axios.get('https://api.github.com/users/AustinGreer')
+.then(res => {
+  debugger
+})
+.catch(err => {
+  debugger
+})
+.then(() => {
+  console.log("it fired")
+})
+
+
 /*
   STEP 2: Inspect and study the data coming back, this is YOUR
     github info! You will need to understand the structure of this
@@ -49,6 +62,76 @@ const followersArray = [];
       </div>
     </div>
 */
+const userCard = (userObj) => {
+  // destructure userObj
+  const { 
+    userImg, 
+    userName, 
+    name, 
+    userLocation, 
+    userProfile, 
+    userFollowers, 
+    userFollowing, 
+    userBio 
+  } = userObj
+
+  //create all elements --> add classes, content, and attributes
+  // 1. card container
+  const card = document.createElement('div')
+  card.classList.add('card')
+
+  // 2. card img
+  const img = document.createElement('img')
+  img.setAttribute('src', userImg)
+
+  // 3. info container
+  const cardInfo = document.createElement('div')
+  cardInfo.classList.add('card-info')
+
+  // 4. card users name
+  const cardName = document.createElement('h3')
+  cardName.classList.add('name')
+  cardName.textContent = name
+
+  // 5. users username
+  const username = document.createElement('p')
+  username.classList.add('username')
+  username.textContent = userName
+
+  // 6. users location
+  const location = document.createElement('p')
+  location.textContent = `Location: ${userLocation}`
+
+  // 7. user profile container
+  const profileContainer = document.createElement('p')
+  profileContainer.textContent = 'Profile:'
+
+  // 8. profile link
+  const profile = document.createElement('a')
+  profile.setAttribute('href', userProfile)
+  profile.textContent = userProfile
+
+  // 9. user followers
+  const followers = document.createElement('p')
+  followers.textContent = `Followers: ${userFollowers}`
+
+  // 10. user following
+  const following = document.createElement('p')
+  following.textContent = `Following: ${userFollowing}`
+
+  // 11. user bio
+  const bio = document.createElement('p')
+  bio.textContent = `Bio: ${userBio}`
+
+
+
+  // structure elements
+  card.append(img, cardInfo)
+  cardInfo.append(cardName, username, location, profileContainer, followers, following, bio)
+  profileContainer.appendChild(profile)
+
+  return card
+}
 
 /*
   List of LS Instructors Github username's:
@@ -57,4 +140,13 @@ const followersArray = [];
     justsml
     luishrd
     bigknell
+
+    userImg, 
+    userName, 
+    name, 
+    userLocation, 
+    userProfile, 
+    userFollowers, 
+    userFollowing, 
+    userBio
 */
